2010cac67a5b9e7193c7ecc6d0ca577239339df7
Ryan Romero <romeroryan01@gmail.com>
2020-04-26 14:12:57 -0700

    Preliminary

--- src/Components/EveMarketFunctions/AXIOS.js
+++ src/Components/EveMarketFunctions/AXIOS.js
@@ -1,8 +1,12 @@
 import React, {Component} from "react";
 const axios = require('axios').default;
 
-
-
+//Eve ID
+//Jita
+const jita_region = 10000002;
+const jita_station = 30000142;
+//Eve Routes
+const marketOp = `https://esi.evetech.net/v1/markets/${jita_region}/orders/?datasource=tranquility&order_type=buy&page=1&type_id=1`;
 
 class UsersComponent extends Component {
     constructor(props) {
@@ -15,14 +19,12 @@ class UsersComponent extends Component {
     componentDidMount() {
         axios.get(` https://esi.evetech.net/latest/markets/groups/?datasource=tranquility`)
             .then(json => this.setState({ itemID: json.data, done: true }))
+            .then(this.returnUsers(), )
     }
     returnUsers(){
         let string = '';
-        this.state.itemID.forEach(item => string += '<div>'+ item + '</div>');
-        return(
-
-            string
-            )
+        this.state.itemID.forEach(item => string += `<div>`+ item + '</div>');
+        console.log(this.state.itemID)
     }
     render() {
         if(!this.state.done) {
--- src/Components/Station.js
+++ src/Components/Station.js
@@ -1,6 +1,12 @@
 import React, {Component} from "react";
 import {Tabs, Tab, Grid, Cell, Card, CardTitle,CardText, CardActions, Button, CardMenu, IconButton} from "react-mdl";
 
+
+
+/*
+jita_region_id = 10000002
+amarr_region_id = 10000043
+*/
 class BuyStation extends Component {
     constructor(props) {
         super(props);

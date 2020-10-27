92852a7729ac8938678acf6b559d76485d964a1e
Ryan Romero <romeroryan01@gmail.com>
2020-10-27 10:44:41 -0700

    commit

--- backend/models/itemInfo.js
+++ backend/models/itemInfo.js
@@ -7,12 +7,18 @@ const itemSchema = new Schema({
     item_id: {type:Number, required:true},
     item_name: {type: String},
     item_graphic: [{type:String}, {type:Number}],
-    
-    
 }, {
     timestamps:true,
 });
-
 const ItemInfo = mongoose.model('ItemInfo', itemSchema);
 
-module.exports = ItemInfo; 
+const stationSchema = new Schema({
+    station_region:{type:Number, required:true},
+    station_id:{type:Number, required:true},
+    station_name:{type:String, required:true}
+
+});
+
+const StationInfo = mongoose.model('StationInfo', stationSchema);
+
+module.exports = [ItemInfo, StationInfo]; 
